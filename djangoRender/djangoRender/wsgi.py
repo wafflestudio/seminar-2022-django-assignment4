054317ac7cd6b968d7c40904a0be898ce4801239
sungmin <sungsung718@gmail.com>
2022-11-19 20:40:24 +0900

    wsgi 파일 경로 수정 ver.2

--- djangoRender/djangoRender/wsgi.py
+++ djangoRender/djangoRender/wsgi.py
@@ -11,7 +11,8 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('/djangoRender/djangoRender')
+sys.path.append('/home/djangoRender')
+sys.path.append('/home/djangoRender/djangoRender')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoRender.settings')
 
