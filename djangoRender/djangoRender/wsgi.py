77213b3cd6e2c6a2f5e2b0f9cc1cfdaeebacbf0b
sungmin <sungsung718@gmail.com>
2022-11-19 21:08:54 +0900

    wsgi 파일 경로 수정 ver.5
    원래대로 한 다음 render root directory을 project명으로 변경

--- djangoRender/djangoRender/wsgi.py
+++ djangoRender/djangoRender/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoRender.djangoRender.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoRender.settings')
 
 application = get_wsgi_application()
