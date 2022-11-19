dd3f4ee8831642caad467a6d4bbec2fd11efdd17
sungmin <sungsung718@gmail.com>
2022-11-19 23:50:37 +0900

    Custom command 생성
    5년 동안 로그인하지 않은 사용자 정보 삭제

--- djangoRender/djangoRender/settings.py
+++ djangoRender/djangoRender/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'melon.apps.MelonConfig',
 ]
 
 LOGGING = {
@@ -67,8 +68,7 @@ ROOT_URLCONF = 'djangoRender.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
