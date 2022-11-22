912b72774be6d07f4a43efbeb609d4d4d030390b
bean109 <auddl1020@gmail.com>
2022-11-22 23:12:24 +0900

    management code updated

--- render/management/commands/deactivate_users.py
+++ render/management/commands/deactivate_users.py
@@ -10,5 +10,6 @@ class Command(BaseCommand):
 
     def handle(self, args, **options):
         qs = User.objects.filter(last_login__lt=timezone.now() - timedelta(months=3))
-        # qs의 유저들에게 비밀번호 변경 메일을 보낸다.
-        self.stdout.write(self.style.SUCCESS('Seccessfully sent mails to users "%d"' % qs.count()))
+        for user in qs:
+            user.email_user("Change your password", "Please change you password")
+        self.stdout.write(self.style.SUCCESS('Successfully sent mails to users "%d"' % qs.count()))
