f503bba3ea54f408d957487ce6d12d5a6cd181a5
BDlhj <akk808@naver.com>
2022-11-24 20:28:28 +0900

    hide django_secret_key

--- djangoRender/djangoRender/settings.py
+++ djangoRender/djangoRender/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yhss86yj&*+@r56z$996c4&d^m1#$$j+7d)v5h3au#u7+19lqr'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
