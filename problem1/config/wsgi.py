56553d1dec81b1bf5295b8a211679ae5f503b74a
JangSeungHyun <jang1751@snu.ac.kr>
2022-11-25 02:35:40 +0900

    하위폴더명 변경

--- problem1/config/wsgi.py
+++ problem1/config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for problem1 project.
+WSGI config for config project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.py')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
