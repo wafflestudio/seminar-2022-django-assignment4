c9a78ed73888fc8567515c452e64e281af36a9a0
serverrepairman <gustjwkd1007@gmail.com>
2022-11-27 03:08:59 +0900

    add ALLOWED_HOSTS option

--- renderPractice/renderPractice/settings.py
+++ renderPractice/renderPractice/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+ALLOWED_HOSTS = ['seminar-2022-django-assignment4-problem-1.onrender.com']
