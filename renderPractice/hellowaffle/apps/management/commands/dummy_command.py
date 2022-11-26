583e7a5a2029e75fa03c3b55aa78dd8bc407768b
serverrepairman <gustjwkd1007@gmail.com>
2022-11-27 03:26:15 +0900

    command update

--- renderPractice/hellowaffle/apps/management/commands/dummy_command.py
+++ renderPractice/hellowaffle/apps/management/commands/dummy_command.py
@@ -5,4 +5,5 @@ class Command(BaseCommand):
     help = 'test command!'
 
     def handle(self, *args, **options):
+        print("Hello, serverrepairman")
         self.stdout.write(self.style.SUCCESS('Hello, cronjob and commands!'))
