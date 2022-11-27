02d0af8f0a4e4e85c5d1e39d28722246e2f35657
JangSeungHyun <jang1751@snu.ac.kr>
2022-11-27 17:59:44 +0900

    custom command 추가 및 manage.py config.settings로 수정

--- problem1/config/settings.py
+++ problem1/config/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
