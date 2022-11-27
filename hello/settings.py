ad2e1f37838046ff40fa631948bf6caeb5a557d6
Jinan <jinanjeong@snu.ac.kr>
2022-11-28 00:58:25 +0900

    implement problem1

--- hello/settings.py
+++ hello/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -67,7 +68,7 @@ ROOT_URLCONF = 'hello.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'hello', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
