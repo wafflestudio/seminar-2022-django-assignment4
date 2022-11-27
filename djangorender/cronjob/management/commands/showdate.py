86e5a3727d0d6b69a0ec6ca39342c5b89e45d97c
kimseoyeon <yeomri0224@gmail.com>
2022-11-27 18:40:16 +0900

    commands 수정

--- djangorender/cronjob/management/commands/showdate.py
+++ djangorender/cronjob/management/commands/showdate.py
@@ -1,8 +1,8 @@
 from django.core.management.base import BaseCommand
-import datetime
+from django.utils import timezone
 
 class Command(BaseCommand):
     help = '오늘의 날짜 표시'
 
     def handle(self, *args, **options):
-       self.stdout.write(f"{datetime.datetime.now()}")
+       self.stdout.write(f"{timezone.now()}")
